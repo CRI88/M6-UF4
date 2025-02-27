d05c080d587648ce328d54e27a8920831ee352e0
CRI88 <christianrieigl@monlau.com>
2025-02-27 17:05:43 +0100

    add admin and products components with routing; update navbar links and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { InitialpageComponent } from './components/initialpage/initialpage.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { Component } from '@angular/core';
+import { ProductsComponent } from './components/products/products.component';
 
 export const routes: Routes = [
     { path: '', component: InitialpageComponent },
+    { path: 'admin', component: AdminComponent},
+    { path: 'products', component: ProductsComponent},
 ];
