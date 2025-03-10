c0b8b32097f47197fa0c20c1ec8789a2827af29c
CRI88 <christianrieigl@monlau.com>
2025-03-10 18:39:34 +0100

    primera preparación de vistas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,16 @@ import { InitialpageComponent } from './components/initialpage/initialpage.compo
 import { AdminComponent } from './components/admin/admin.component';
 import { Component } from '@angular/core';
 import { ProductsComponent } from './components/products/products.component';
+import { AccountComponent } from './components/account/account.component';
+import { ProductviewComponent } from './components/productview/productview.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: InitialpageComponent },
+    { path: 'initial', component: InitialpageComponent},
+    { path: 'register', component: RegisterComponent},
     { path: 'admin', component: AdminComponent},
     { path: 'products', component: ProductsComponent},
+    { path: 'account', component: AccountComponent},
+    { path: 'productview', component: ProductviewComponent}
 ];
